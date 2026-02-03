77eb90e8fcf26e2448273845a9f65bead75fd766
root <root@srv1322618.hstgr.cloud>
2026-02-03 20:26:45 +0000

    Remove other Film Duel versions, keep only IMDb Top 100

---
+++ fix_posters.js
@@ -0,0 +1,8 @@
+// Prüfe welche Poster fehlen und erstelle Fallback-Logik
+const missingPosters = [
+  '6y8wg7a7P4D5z3u5m5v5n5v5n5.jpg',
+  '8f9d8f9d8f9d8f9d8f9d8f9d8f.jpg',
+  // ... viele mehr sind Platzhalter
+];
+
+// Lösung: Verwende einen zuverlässigen Platzhalter-Service
